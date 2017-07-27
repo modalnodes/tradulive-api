757d9c08389f4e0dd5ec1b736cb05dc3a0e750b9
Marco Montanari <sirmmo@gmail.com>
2017-07-27 08:52:38 +0000

    updates

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Language)
+admin.site.register(LanguageLevel)
+admin.site.register(CertificationIssuer)
+admin.site.register(Certification)
+admin.site.register(TranslatorProfile)
+admin.site.register(TranslatorLanguage)
+admin.site.register(TranslatorMicrovoc)
+admin.site.register(TranslatorMode)
+admin.site.register(CustomerProfile)
+admin.site.register(CustomerUser)
+admin.site.register(CreditAssignment)
+admin.site.register(MicroVocabulary)
+admin.site.register(Transaction)
+admin.site.register(Appointment)
+admin.site.register(AppointmentClosure)
+admin.site.register(AppointmentEvaluation)
+admin.site.register(TranslatorInvoice)
